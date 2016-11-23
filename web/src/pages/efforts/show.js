3130a44db67ef3bfd949ae52ddbb6c8a4bbdf047
Joshua Smith <joshua.aaron.smith17@gmail.com>
2016-11-23 14:15:59 -0500

    added team functionality

--- web/src/pages/efforts/show.js
+++ web/src/pages/efforts/show.js
@@ -3,8 +3,13 @@ const {Link, Redirect} = require('react-router')
 const xhr = require('xhr')
 
 const ShowEffort = React.createClass({
-    getInitialState() {
-        return {effort: {}, removed: false}
+    getInitialState: function() {
+        return {
+            effort: {
+                members: []
+            },
+            persons: []
+        };
     },
     componentDidMount() {
         xhr.get('http://localhost:4000/efforts/' + this.props.params.id, {
@@ -12,7 +17,16 @@ const ShowEffort = React.createClass({
         }, (err, response, effort) => {
             if (err)
                 return console.log(err.message)
+            if (!effort.members)
+                effort.members = []
             this.setState({effort})
+        })
+        xhr.get('http://localhost:4000/persons', {
+            json: true
+        }, (err, response, persons) => {
+            if (err)
+                return console.log(err.message)
+            this.setState({persons})
         })
     },
     handleRemove(e) {
@@ -32,6 +46,46 @@ const ShowEffort = React.createClass({
             })
         }
     },
+    addMember(person) {
+        return (e) => {
+            let members = this.state.effort.members.filter(member => member.id !== person.id)
+            let effort = {
+                ...this.state.effort
+            }
+            effort.members = [
+                person, ...members
+            ]
+            this.setState({effort})
+
+        }
+    },
+    removeMember(m) {
+        return (e) => {
+            let members = this.state.effort.members.filter(member => member.id !== m.id)
+            let effort = {
+                ...this.state.effort
+            }
+            effort.members = members
+            this.setState({effort})
+
+        }
+    },
+    updateTeam(e) {
+        e.preventDefault()
+        // this.props.put('efforts', this.state.effort.id, this.state.effort, (err, res) => {
+        //     if (err)
+        //         return console.log(err.message)
+        //     alert('Successfully updated team!')
+        // })
+        xhr.put('http://localhost:4000/efforts/' + this.state.effort.id, {
+            json: this.state.effort
+        }, (err, response, body) => {
+            if (err)
+                return console.log(err.message)
+            this.setState({success: true})
+            console.log(body)
+        })
+    },
     render() {
         return (
             <div className="avenir fw1 tc">
@@ -46,13 +100,40 @@ const ShowEffort = React.createClass({
                     <h5 className="fw1">{this.state.effort.desc}</h5>
                     <h5 className="fw1">{this.state.effort.start + ' - '}{this.state.effort.end}</h5>
                     <h5 className="fw1">Team Lead: {this.state.effort.teamLead}</h5>
+                    <div>
+                        <div className="">
+                            <h3 className="fw1 f4">Available Persons</h3>
+                            <div>
+                                <ul>
+                                    {this.state.persons.map(p => <li key={p.id}>
+                                        {p.firstName + ' ' + p.lastName}
+                                        <button onClick={this.addMember(p)}>Add Member</button>
+                                    </li>)}
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="">
+                            <h3 className="fw1 f4">This Effort's Team</h3>
+                            <div>
+                                <ul>
+                                    {this.state.effort.members.map(m => <li key={m.id}>
+                                        {m.firstName + ' ' + m.lastName}
+                                        <button onClick={this.removeMember(m)}>Remove</button>
+                                    </li>)}
+                                </ul>
+                                <button onClick={this.updateTeam}>Save Updated Team</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <hr className="w-10"/>
-                <button className="f6 fw1 link dim br2 ba ph3 pv2 mt3 mb2 db black center">
-                    <Link className="no-underline black hover-bg-moon-gray mb2" to={`/efforts/${this.state.effort.id}/edit`}>Edit Effort</Link>
-                </button>
-                <button className="f6 fw1 link dim br2 ba ph3 pv2 mt2 mb2 db black center" onClick={this.handleRemove}>Remove Effort</button>
-                <Link className="no-underline black hover-bg-moon-gray" to="/efforts">Return</Link>
+                <div className="db">
+                    <hr className="w-10"/>
+                    <button className="f6 fw1 link dim br2 ba ph3 pv2 mt3 mb2 db black center">
+                        <Link className="no-underline black hover-bg-moon-gray mb2" to={`/efforts/${this.state.effort.id}/edit`}>Edit Effort</Link>
+                    </button>
+                    <button className="f6 fw1 link dim br2 ba ph3 pv2 mt2 mb2 db black center" onClick={this.handleRemove}>Remove Effort</button>
+                    <Link className="no-underline black hover-bg-moon-gray" to="/efforts">Return</Link>
+                </div>
             </div>
         )
     }
--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,11 +1,15 @@
 const React = require('react')
+const {Banner, Heading} = require('rebass')
 
 const Home = React.createClass({
     render() {
         return (
             <div className="avenir fw1 tc">
-                <h3 className="fw1 f2">Home</h3>
-
+                <Banner align="center" backgroundImage="https://static.pexels.com/photos/14676/pexels-photo-14676.png">
+                    <Heading level={2} size={0}>
+                        Relief Tracker
+                    </Heading>
+                </Banner>
             </div>
         )
     }
